4d554101464fb5e2e2bbf41da287b5f6cc7f380f
Matt Newcomer <47364420+panevain@users.noreply.github.com>
2023-11-06 22:21:43 -0500

    Chapter 7

--- jlox/src/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/com/craftinginterpreters/lox/Parser.java
@@ -7,6 +7,7 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Parser {
     private final List<Token> tokens;
     private int current = 0;
+
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
