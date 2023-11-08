0c3c20cf88a019be4d886e5c5a1a54d5f66c43b1
Matt Newcomer <47364420+panevain@users.noreply.github.com>
2023-11-08 17:49:02 -0500

    Chapter 11

--- jlox/src/com/craftinginterpreters/lox/Environment.java
+++ jlox/src/com/craftinginterpreters/lox/Environment.java
@@ -15,6 +15,20 @@ public class Environment {
         this.enclosing = enclosing;
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
+
+    void assignAt(int distance, Token name, Object value){
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
@@ -41,5 +55,9 @@ public class Environment {
         if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
     }
 }
